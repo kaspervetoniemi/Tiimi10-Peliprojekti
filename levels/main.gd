ee49709b24e14ab98df34928066512c64059ac09
Ilari-A <Ilari.altio@tuni.fi>
2026-05-05 10:17:19 +0300

    Added an enemy randomizer to the grid
    Lisätty vastustajien randomointi gridiin johon ne spawnaa

--- levels/main.gd
+++ levels/main.gd
@@ -3,15 +3,33 @@ extends Node2D
 @export var alien_scene: PackedScene
 var alien_direction = 1
 var alien_speed = 50
+var alien_scenes: Array[PackedScene] = [
+	preload("res://enemys/rapu.tscn"),
+	preload("res://enemys/mustekala.tscn"),
+	preload("res://enemys/turska.tscn")
+]
 
 var bg_music = preload("res://sounds/backgroundmusic.wav") # Update with your file's name!
 var music_player = AudioStreamPlayer.new()
 
 func _ready():
-	# Spawn a 10x4 grid of aliens when the game starts
+	# Create a variable to remember the last monster we picked
+	var last_scene: PackedScene = null
+	
+	# Spawn a 10x4 grid of aliens
 	for row in range(4):
+		
+		var available_scenes = alien_scenes.duplicate()
+		
+		if last_scene != null:
+			available_scenes.erase(last_scene)
+			
+		var row_scene = available_scenes.pick_random()
+		
+		last_scene = row_scene
+		
 		for col in range(10):
-			var alien = alien_scene.instantiate()
+			var alien = row_scene.instantiate()
 			# Space them out nicely
 			alien.position = Vector2(col * 60 + 100, row * 40 + 50) 
 			add_child(alien)
